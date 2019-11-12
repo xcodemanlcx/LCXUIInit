84ab60080b4dac643860cf195745846c8b356547
lcx <leichunxiang58@163.com>
2019-11-12 22:34:43 +0800

    project init

---
+++ LCXUIInit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCXUIInit
+//
+//  Created by leichunxiang on 2019/11/12.
+//  Copyright © 2019 lcx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LCXUIInit/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LCXUIInit
+//
+//  Created by leichunxiang on 2019/11/12.
+//  Copyright © 2019 lcx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
